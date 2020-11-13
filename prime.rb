b92f60750291f593ac64c2859bf71192ede74258
Kyren Barrios <kyrenbarrios@gmail.com>
2020-11-13 13:17:48 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
 # Add  code here!
 def prime?(num)
-    n = 2
-    while num < 2
-      return false if num % n == 0 
-      n += 1
-    end
-    true
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0 || num < 2
   end
+  true
+end
