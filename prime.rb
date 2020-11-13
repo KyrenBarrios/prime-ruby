72ea659fac47f6a4fe767945b6d635a90c7aabbb
Kyren Barrios <kyrenbarrios@gmail.com>
2020-11-13 15:52:42 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
-# Add  code here!
-def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0 || num < 2
-  end
+def prime?(number)
+  return false if !number.integer?
+  return false if number < 2
+  return true if number == 2
+
+  (2..number-1).each {|int| return false if number % int == 0}
+
   true
 end
+
